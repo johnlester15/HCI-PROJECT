40e6211d7865988bda1f07116443cbec056e15de
johnlester15 <165919597+johnlester15@users.noreply.github.com>
2024-05-08 21:28:32 +0800

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-function func() {
-    var email = document.getElementById("Username").value;
-    var pass = document.getElementById("Password").value;
-    if ((email === "BSIT@gmail.com" && pass === "123") || (email === "John.defensor@hcdc.edu.ph" && pass === "123") || (email === "admin" && pass === "123")) {
-        window.location.assign("index.html");
-    } else {
-        alert("Invalid username or password. Please try again.");
-    }
-}
